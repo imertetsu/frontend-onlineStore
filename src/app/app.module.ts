b584514c5615676ce8936a165dee3ea926719db5
unknown <imertpro@gmail.com>
2023-05-19 20:39:41 -0400

    sections added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,9 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgClass
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  buttonActive:boolean = false;
+  buttonClassInactive:string = "text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium";
+  buttonClassActive:string = "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium";
+  isActive: number = 0;
+
+
+  setActive(buttonId: number) {
+    this.buttonClassInactive = "text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium";
+    this.isActive = buttonId;
+  }
 }
+
